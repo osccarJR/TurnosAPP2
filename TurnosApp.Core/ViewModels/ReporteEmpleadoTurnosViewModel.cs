f2537b24f73139016d805f6c00171284343ad363
Emilio Albornoz <oscaremilioalbornoz@hotmail.com>
2025-07-07 00:29:58 -0500

    Primer commit del proyecto TurnosAPP

---
+++ TurnosApp.Core/ViewModels/ReporteEmpleadoTurnosViewModel.cs
@@ -0,0 +1,9 @@
+namespace TurnosApp.Core.ViewModels
+{
+    public class ReporteEmpleadoTurnosViewModel
+    {
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public int CantidadTurnos { get; set; }
+    }
+}
---
+++ TurnosApp.Infrastructure/Migrations/20250603004941_AddMesAnioToDisponibilidades.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TurnosApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMesAnioToDisponibilidades : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Anio",
+                table: "Disponibilidades",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Mes",
+                table: "Disponibilidades",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Anio",
+                table: "Disponibilidades");
+
+            migrationBuilder.DropColumn(
+                name: "Mes",
+                table: "Disponibilidades");
+        }
+    }
+}
